b0f43c342c382afc0e1b586469163247714f4aab
shivanshi.rusia <shivanshi.rusia@tothenew.com>
2024-09-20 01:09:05 +0530

    NativeEventEmitter in ios

---
+++ ios/RNEventEmitter.m
@@ -0,0 +1,12 @@
+#import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
+
+@interface RCT_EXTERN_MODULE(RNEventEmitter, RCTEventEmitter)
+
+RCT_EXTERN_METHOD(supportedEvents)
+
+
+@end
+
+
